a2c658952f96ff1d811e1c4e311d466c1389b97c
stivostenberg <megastiv@stiv.com>
2015-06-15 09:59:49 -0700

    Got properties working!

--- src/BeanBagger/BeanBagger.java
+++ src/BeanBagger/BeanBagger.java
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 package BeanBagger;
-import java.lang.management.ManagementFactory;
-import java.util.Iterator;
 import java.util.Set;
 import java.io.File;
 import java.io.IOException;
@@ -15,13 +13,7 @@ import java.util.Map;
 import java.util.Properties;
 
 import javax.management.*;
-import javax.management.MBeanServer;
 import javax.management.ObjectInstance;
-import javax.management.remote.*;
-
-
-
-
 
 import com.sun.tools.attach.AttachNotSupportedException;
 import com.sun.tools.attach.VirtualMachine;
@@ -45,7 +37,7 @@ public class BeanBagger {
      */
     public static void main(String[] args) throws Exception {
         
-        if(args[0].length()>0  )
+        if(args.length>0 && args[0].length()>0  )
         {
             TARGET=args[0];
             if(args.length>1)
@@ -60,8 +52,15 @@ public class BeanBagger {
         else
         {
             System.out.println("Beanbagger {process} {bean}");
-            System.out.println("process: Process Name to try to connect to:");
-            System.out.println("bean:  restrict data to just one bean. Default is all beans ");
+            System.out.println("  process: Process Name to try to connect to:");
+            System.out.println("  bean:  optional, restrict data to just one bean. Default is all beans ");
+            System.out.println("\nProcesses found:");
+            List<VirtualMachineDescriptor> list = VirtualMachine.list();
+            for (VirtualMachineDescriptor vmd: list)
+            {
+                System.out.println("   "+ vmd.displayName());
+            }
+            System.exit(1);
         }
         
                
@@ -86,7 +85,7 @@ public class BeanBagger {
                } 
                else
                {
-                  listofjvs+=DN + "\n" ;
+                  listofjvs+=DN + "  \n" ;
                }
             }
             catch (IOException | AttachNotSupportedException e) {
@@ -98,7 +97,7 @@ public class BeanBagger {
                     if(!gotit)//If we dont find the instance.
             {
               System.out.println("Target instance not found: " + TARGET); 
-              System.out.println("Found: " + listofjvs );
+              System.out.println("Found instances: " + listofjvs );
               System.exit(1);
             }
         
@@ -106,14 +105,10 @@ public class BeanBagger {
 myJMXconnector = getLocalConnection(VirtualMachine.attach(TARGETDESCRIPTOR));// Connects to the process containing our beans
 MBeanServerConnection myJMXConnection = myJMXconnector.getMBeanServerConnection(); //Connects to the MBean server for that process.
 
-
 System.out.println("Beans found: " +  myJMXConnection.getMBeanCount());
 
 String getDefaultDomain = myJMXConnection.getDefaultDomain();
 String[] getDomains=myJMXConnection.getDomains();
-
-
-
 
 
 Set<ObjectInstance> beans = myJMXConnection.queryMBeans(null, null);
@@ -122,10 +117,9 @@ for( ObjectInstance instance : beans )
     String daclassname = instance.getClassName();
     if(daclassname.contains(BEAN) || BEAN.contentEquals("*"))
     {
-    System.out.println("Processing me a bean: " + daclassname);   
+    System.out.println("Processing me a bean: " + daclassname + "\n  Attributes:");   
 
     MBeanInfo info = myJMXConnection.getMBeanInfo( instance.getObjectName() );
-    
     MBeanAttributeInfo[] myAttributeArray = info.getAttributes();
     for(MBeanAttributeInfo thisAttributeInfo : myAttributeArray)
     {
@@ -134,27 +128,41 @@ for( ObjectInstance instance : beans )
         //String mydesc = thisAttributeInfo.getDescription();
         String mytype = thisAttributeInfo.getType();
 
-        
-        
-        //attvalue = (String)myJMXConnection.getAttribute(instance.getObjectName(), );
-        
-        System.out.println("  Name:" + myname + "  Type:" + mytype + "  Value:"  + attvalue); 
+        try{
+        switch (mytype) {
+            case "String":
+                attvalue = (String)myJMXConnection.getAttribute(instance.getObjectName(), myname );
+                System.out.println("  Name:" + myname + "  Type:" + mytype + "  Value:"  + attvalue);
+                break;
+            case "java.lang.String":
+                attvalue = (String)myJMXConnection.getAttribute(instance.getObjectName(), myname );
+                System.out.println("  Name:" + myname + "  Type;" + mytype + "  Value:"  + attvalue);
+                break;    
+            case "boolean":
+                attvalue = myJMXConnection.getAttribute(instance.getObjectName(), myname ).toString();
+                System.out.println("  Name:" + myname + "  Type:" + mytype + "  Value:"  + attvalue);
+                break;
+            case "int":
+                attvalue = myJMXConnection.getAttribute(instance.getObjectName(), myname ).toString();
+                System.out.println("  Name:" + myname + "  Type:" + mytype + "  Value:"  + attvalue);
+                break;  
+            case "long":
+                attvalue = myJMXConnection.getAttribute(instance.getObjectName(), myname ).toString();
+                System.out.println("  Name:" + myname + "  Type:" + mytype + "  Value:"  + attvalue);
+                break;     
+            default:
+                attvalue = "Unsupported type";
+                System.out.println("  Name:" + myname + "  Type:" + mytype + "  Value:"  + attvalue);
+                break;  
+        }
+        }
+        catch(UnsupportedOperationException | RuntimeMBeanException ex)
+                {
+                attvalue = "Unsupported Operation";
+                System.out.println("  Name:" + myname + "  Type:" + mytype + "  Value:"  + attvalue);  
+                }
+
     }
-
-    
-    
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     }
    
  
@@ -166,17 +174,7 @@ for( ObjectInstance instance : beans )
 
 
 
-
-
-
-
-
-
-
-
-
-
-System.out.println("Th th thats all folks!");         
+System.out.println("Stiv's Beanbagger Finished");         
     }
     
 static JMXConnector getLocalConnection(VirtualMachine vm) throws Exception {
